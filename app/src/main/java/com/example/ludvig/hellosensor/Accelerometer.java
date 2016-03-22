37abdc93c15e101cdc28ed74e6eee716b48166ca
luddeakesson <goolfboll91>
2016-03-22 13:18:50 +0100

    första utkasst klart

--- app/src/main/java/com/example/ludvig/hellosensor/Accelerometer.java
+++ app/src/main/java/com/example/ludvig/hellosensor/Accelerometer.java
@@ -4,21 +4,33 @@ import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by Ludvig on 2016-03-22.
  */
 public class Accelerometer extends Activity implements SensorEventListener{
 
+    Sensor accelerometer;
+    SensorManager sm;
+    TextView acceleration;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accelerometer);
+
+        sm = (SensorManager)getSystemService(SENSOR_SERVICE);
+        accelerometer = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sm.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+        acceleration = (TextView)findViewById(R.id.acceleration);
     }
     @Override
     public void onSensorChanged(SensorEvent event) {
-
+        acceleration.setText("X: "+event.values[0] + "\nY: "+event.values[1] + "\nZ: " + event.values[2]);
     }
 
     @Override
